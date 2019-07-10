e5d158c363a9a89078683a145fc9ce76f285153f
Jmardura <jarduraterra@gmail.com>
2019-07-10 18:41:22 +0200

    Fase 2 implementada

--- VehiclesProject/src/com/vehicles/project/Main.java
+++ VehiclesProject/src/com/vehicles/project/Main.java
@@ -7,8 +7,19 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Introdueix la matricula del cotxe: ");
-		String matricula = scanner.nextLine();
+		
+		boolean correcte = false;
+		String matricula;
+		do{
+			System.out.println("Introdueix la matricula del cotxe: ");
+			matricula = scanner.nextLine();
+			//if(matricula.length()<7||matricula.substring(0,3).contains(("[a-zA-Z]+"))||matricula.substring(4,7).contains("[0-9]+")) {
+			if(Character.isDigit(matricula.charAt(0))&&Character.isDigit(matricula.charAt(1))&&Character.isDigit(matricula.charAt(2))&&Character.isDigit(matricula.charAt(3))&&!Character.isDigit(matricula.charAt(4))&&!Character.isDigit(matricula.charAt(5))&&!Character.isDigit(matricula.charAt(6))) {	
+				correcte = true;
+			}else {
+				System.out.println("la matrícula ha de contenir 4 digits + 3 lletres");
+			}
+		}while(!correcte);
 		System.out.println("Introdueix la marca del cotxe: ");
 		String marca = scanner.nextLine();
 		System.out.println("Introdueix el color del cotxe: ");
@@ -21,8 +32,18 @@ public class Main {
 		List<Wheel> backWheels = new ArrayList<>();
 		System.out.println("Introdueix la marca de les rodes de darrera: ");
 		String marcaRodaTras = scanner.nextLine();
-		System.out.println("Introdueix el diametre de les rodes de darrera: ");
-		String diamRodaTras = scanner.nextLine();
+		boolean marge = false;
+		String diamRodaTras;
+		do {
+			System.out.println("Introdueix el diametre de les rodes de darrera: ");
+			diamRodaTras = scanner.nextLine();
+			double diamDouble= Double.parseDouble(diamRodaTras);
+			if (diamDouble>4 || diamDouble<0.4) {
+				System.out.println("El diametre no pot ser major de 4 ni menor de 0.4");
+			}else {
+				marge= true;
+			}
+		}while (!marge);
 		Wheel trasera = new Wheel(marcaRodaTras,Double.parseDouble(diamRodaTras));
 		Wheel trasera2 = new Wheel(marcaRodaTras,Double.parseDouble(diamRodaTras));
 
@@ -33,9 +54,21 @@ public class Main {
 		System.out.println("Introdueix la marca de les rodes de davant: ");
 		String marcaRodaDav = scanner.nextLine();
 		System.out.println("Introdueix el diametre de les rodes de davant: ");
-		String diamRodaDav = scanner.nextLine();
-		Wheel davant = new Wheel(marcaRodaDav,Double.parseDouble(diamRodaDav));
-		Wheel davant2 = new Wheel(marcaRodaDav,Double.parseDouble(diamRodaDav));
+		boolean marge2 = false;
+		String diamRodaDav;
+		double diamDavDouble;
+		do {
+			System.out.println("Introdueix el diametre de les rodes de davant: ");
+			diamRodaDav = scanner.nextLine();
+			diamDavDouble= Double.parseDouble(diamRodaDav);
+			if (diamDavDouble>4 || diamDavDouble<0.4) {
+				System.out.println("El diametre no pot ser major de 4 ni menor de 0.4");
+			}else {
+				marge2= true;
+			}
+		}while (!marge2);
+		Wheel davant = new Wheel(marcaRodaDav,diamDavDouble);
+		Wheel davant2 = new Wheel(marcaRodaDav,diamDavDouble);
 
 		frontWheels.add(davant);
 		frontWheels.add(davant2);
