6791122c749862fa268f6f184e29871577fb11dc
Joan Manel Ardura Terra <jarduraterra@gmail.com>
2019-07-10 11:27:18 +0200

    vehicles Fase1

---
+++ VehiclesProject/src/com/vehicles/project/Main.java
@@ -0,0 +1,47 @@
+package com.vehicles.project;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Introdueix la matricula del cotxe: ");
+		String matricula = scanner.nextLine();
+		System.out.println("Introdueix la marca del cotxe: ");
+		String marca = scanner.nextLine();
+		System.out.println("Introdueix el color del cotxe: ");
+		String color = scanner.nextLine();
+		
+		Car cotxe1 = new Car(matricula, marca, color);
+		System.out.println(cotxe1);
+		
+		
+		List<Wheel> backWheels = new ArrayList<>();
+		System.out.println("Introdueix la marca de les rodes de darrera: ");
+		String marcaRodaTras = scanner.nextLine();
+		System.out.println("Introdueix el diametre de les rodes de darrera: ");
+		String diamRodaTras = scanner.nextLine();
+		Wheel trasera = new Wheel(marcaRodaTras,Double.parseDouble(diamRodaTras));
+		backWheels.add(trasera);
+		backWheels.add(trasera);
+		
+		List<Wheel> frontWheels = new ArrayList<>();
+		System.out.println("Introdueix la marca de les rodes de davant: ");
+		String marcaRodaDav = scanner.nextLine();
+		System.out.println("Introdueix el diametre de les rodes de davant: ");
+		String diamRodaDav = scanner.nextLine();
+		Wheel davant = new Wheel(marcaRodaDav,Double.parseDouble(diamRodaDav));
+		frontWheels.add(davant);
+		frontWheels.add(davant);
+		
+		try {
+			cotxe1.addWheels(frontWheels, backWheels);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(cotxe1);
+	}
+}
