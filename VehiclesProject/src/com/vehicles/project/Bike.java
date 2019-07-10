cfff37a1cb728e69d46bc0cef8fbb825799cf2d1
Jmardura <jarduraterra@gmail.com>
2019-07-10 19:06:32 +0200

    implementada fase3: moto

--- VehiclesProject/src/com/vehicles/project/Bike.java
+++ VehiclesProject/src/com/vehicles/project/Bike.java
@@ -1,9 +1,18 @@
 package com.vehicles.project;
 
+import java.util.List;
+
 public class Bike extends Vehicle {
 
 	public Bike(String plate, String brand, String color) {
 		super(plate, brand, color);
+	}
+	
+	public void addFrontWheel(Wheel wh) {
+		this.wheels.add(wh);
+	}
+	public void addRearWheel(Wheel wh) {
+		this.wheels.add(wh);
 	}
 
 }
